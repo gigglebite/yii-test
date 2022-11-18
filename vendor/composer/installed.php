9969727df637b46f9d975e87c16b7408eb33a49c
gigglebite <candelaria.aleeyajenzen@auf.edu.ph>
2022-11-18 13:41:25 +0800

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'yiisoft/yii' => array(
+            'pretty_version' => '1.1.26',
+            'version' => '1.1.26.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yiisoft/yii',
+            'aliases' => array(),
+            'reference' => 'bda32f68713d4501d3fe9571e09e4b2fa06c53b7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
